919840b7de1737d48e32078fdf89af6670919a0d
Matt Hughes <mhughes.ipod@gmail.com>
2023-03-14 20:09:20 -0700

    api

---
+++ src/pages/api/rs3/player.ts
@@ -0,0 +1,29 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { getPlayerData } from "../../../server/common/stat-services";
+
+import { prisma } from "../../../server/db/client";
+const ctx = { prisma };
+
+const player = async (req: NextApiRequest, res: NextApiResponse) => {
+  // get username from query param
+  const username = req.query.username?.toString();
+  if (!username || username.length > 12) {
+    res.status(400).send("Username is required");
+    return;
+  }
+  const playerGainsResponse = await getPlayerData({ username, ctx });
+
+  if (!playerGainsResponse.success) {
+    if (
+      playerGainsResponse.message === "Player not found on official hiscores"
+    ) {
+      res.status(404).send(playerGainsResponse);
+    }
+
+    res.status(500).send(playerGainsResponse);
+  }
+
+  res.status(200).send(playerGainsResponse);
+};
+
+export default player;
