c9385b89242b8f63196c74e82090b7b7fdb9d014
Matt Hughes <matthew.hughes@mainstreet.com>
2023-08-06 21:23:55 -0700

    avatar image retry

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,9 +1,11 @@
+import React, { useState } from "react";
 type props = {
   username: string;
   width?: string;
 };
 
 const Avatar = ({ username, width = "w-full" }: props) => {
+  const [imgKey, setImgKey] = useState(0);
   const formattedUsername = username.split(" ").join("+");
   const url = `https://secure.runescape.com/m=avatar-rs/${formattedUsername}/chat.png`;
 
@@ -13,11 +15,16 @@ const Avatar = ({ username, width = "w-full" }: props) => {
   return (
     <img
       src={url}
+      key={imgKey}
       alt="avatar"
       className={width}
       onError={({ currentTarget }) => {
         currentTarget.onerror = null; // prevents looping
         currentTarget.src = backupUrl;
+        if (imgKey === 0) {
+          currentTarget.src = url;
+          setImgKey(1);
+        }
       }}
     />
   );
