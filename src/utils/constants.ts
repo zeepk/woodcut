5ce601fd6181b08cef14cdec1d7fcd4eee3c2c5f
Matt Hughes <matthew.hughes@mainstreet.com>
2023-08-06 20:50:14 -0700

    necro

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -15,6 +15,9 @@ export const RunescapeImApiBaseUrlRs3 =
   "https://secure.runescape.com/m=hiscore_ironman/index_lite.ws?player=";
 export const RunescapeHcimApiBaseUrlRs3 =
   "https://secure.runescape.com/m=hiscore_hardcore_ironman/index_lite.ws?player=";
+export const RunescapeApiRankedUrlRs3Pre =
+  "https://secure.runescape.com/m=hiscore/ranking.json?table=";
+export const RunescapeApiRankedUrlRs3Post = "&category=0&size=50";
 
 export const RunescapeApiBaseUrlOsrs =
   "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=";
@@ -293,6 +296,31 @@ export const textToIgnore = [
 export const detailsToIgnore = [...levelsToIgnore];
 
 // Other
+const xpTable = [
+  0, 83, 174, 276, 388, 512, 650, 801, 969, 1154, 1358, 1584, 1833, 2107, 2411,
+  2746, 3115, 3523, 3973, 4470, 5018, 5624, 6291, 7028, 7842, 8740, 9730, 10824,
+  12031, 13363, 14833, 16456, 18247, 20224, 22406, 24815, 27473, 30408, 33648,
+  37224, 41171, 45529, 50339, 55649, 61512, 67983, 75127, 83014, 91721, 101333,
+  111945, 123660, 136594, 150872, 166636, 184040, 203254, 224466, 247886,
+  273742, 302288, 333804, 368599, 407015, 449428, 496254, 547953, 605032,
+  668051, 737627, 814445, 899257, 992895, 1096278, 1210421, 1336443, 1475581,
+  1629200, 1798808, 1986068, 2192818, 2421087, 2673114, 2951373, 3258594,
+  3597792, 3972294, 4385776, 4842295, 5346332, 5902831, 6517253, 7195629,
+  7944614, 8771558, 9684577, 10692629, 11805606, 13034431, 14391160, 15889109,
+  17542976, 19368992, 21385073, 23611006, 26068632, 28782069, 31777943,
+  35085654, 38737661, 42769801, 47221641, 52136869, 57563718, 63555443,
+  70170840, 77474828, 85539082, 94442737, 104273167,
+];
+
+export const xpToLevel = (xp: number) => {
+  for (let i = xpTable.length - 1; i >= 0; i--) {
+    if (xp >= xpTable[i]) {
+      return i + 1;
+    }
+  }
+
+  return 1; // Return level 1 if XP is lower than the minimum value in the table
+};
 
 export const verificationWorlds = [
   7, 8, 11, 17, 19, 20, 29, 34, 38, 41, 43, 55, 61, 80, 81, 94, 108, 141,
